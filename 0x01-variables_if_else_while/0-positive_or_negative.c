67fafd21e0c31992ff12aa3900f9806a45030a43
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-11 11:19:24 -0700

    alx-Clang

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -14,11 +15,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		printf("%d is zero", n);
-	else if (n > 0)
-		printf("%d is positive",n);
-	else if (n < 0)
-		printf("%d is negative", n);
+	/* your code goes there */
+	if (!n)
+		printf("%d is zero\n", n);
+	else
+		(n > 0) ? printf("%d is positive\n", n) : printf("%d is negative\n", n);
+
 	return (0);
 }
