d124e7a63ddbaa6328df2a6aeb8b9d81b5df30fb
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-11 11:17:39 -0700

    alx-Clang

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
-		printf(" is zero");
+		printf("%d is zero", n);
 	else if (n > 0)
-		printf(" is positive");
-	else
-		printf(" is negative");
+		printf("%d is positive",n);
+	else if (n < 0)
+		printf("%d is negative", n);
 	return (0);
 }
