930ec41b105c20a43e0f33ba7adda833eefb2fce
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-11 09:59:50 -0700

    alx-c-low-level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-/*
- * main -Entry point
+/**
+ * main - This is the entry point of the code
  *
- * Description: A C program that print with puts function
- *
- * Return: Always 0 (success)
+ * Return - 0 Successful
 */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
