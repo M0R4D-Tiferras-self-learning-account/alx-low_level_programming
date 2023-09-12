df12bf567c12e4dbc32082d80af2f4de87b0911c
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-12 10:38:55 -0700

    alx-function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,8 @@
  * Return: 1 if n is greater than zero, 0 if n is zero,
  *			-1 if less than zero.
 */
-int print_sign(int n)
+int	print_sign(int n)
 {
-	if (n > 0)
-		return (1);
-	else if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (0);
+	_putchar((n > 0) ? '+' : (n < 0) ? '-' : '0');
+	return ((n > 0) ? 1 : (n < 0) ? -1 : 0);
 }
