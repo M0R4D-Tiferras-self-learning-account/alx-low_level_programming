7356fd7cb848e45f1f3b0d287f0ba1f3a4dd40a4
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-12 08:48:27 -0700

    alx-functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include <main.h>
 
-/*
- * function that prints the alphabet, in lowercase, followed by a new line
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  *
  * return - void
- *
- * */
+*/
 void	print_alphabet(void)
 {
-	char 	c;
+	char	c;
 
 	c = 'a';
 	while (c >= 'a' && c <= 'z')
