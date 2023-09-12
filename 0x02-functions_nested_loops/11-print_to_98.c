09b5128a2c32123c730068c32b352861828ab164
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-12 14:35:39 -0700

    alx-functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_to_98 - unction that prints all natural numbers from n to 98
+ *
+ * @n : will be handled
+ *
+ * Return: void
+*/
+
+int	print_to_98(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			if (j == 0)
+				_putchar('0');
+			else if (k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
