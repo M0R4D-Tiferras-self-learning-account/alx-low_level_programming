70d2b7240183fab6df9bd590dd23467bfd440774
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-12 11:34:00 -0700

    alx-function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ *
+ * @x - an intiger will be handled
+ *
+ * @y - an intiger will be handled
+ *
+ * Return : the sum of x and y
+*/
+
+int	add(int x, int y)
+{
+	return (x + y);
+}
