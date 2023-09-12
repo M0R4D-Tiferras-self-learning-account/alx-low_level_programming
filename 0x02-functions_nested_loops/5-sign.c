9d89ad1cc30aea9832e34d93526298c8a4660b89
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-12 10:34:08 -0700

    alx-function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_sign - prints the sign of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero,
+ *			-1 if less than zero.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+}
