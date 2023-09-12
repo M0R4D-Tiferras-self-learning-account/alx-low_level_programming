75829b594df03821dc4fbf6a48c90b84df40ba96
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-12 11:03:34 -0700

    alx-function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,26 @@
 #include <unistd.h>
 
 /**
+ * _abs - a function that computes the absolute value of an integer.
+ *
+ * @n : an initger will be checked
+ *
+ * Return: the absulute value of n
+*/
+
+int	_abs(int n)
+{
+	return ((n < 0) ? -n : n);
+}
+
+/**
  * print_last_digit - function that prints the last digit of a number
  *
  * @n: an intiger will be handled
  *
  * Return: the result
 */
+
 
 int	print_last_digit(int n)
 {
