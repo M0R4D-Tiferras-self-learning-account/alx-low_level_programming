34fbfda3848fa46f11bf22196323a0b35fda0c72
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-14 06:59:49 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: number of times the character _ should be printed
+ *
+ * * Return: nothing
+*/
+void	print_line(int n)
+{
+	int	i;
+
+	i = 0;
+	while (i <= n)
+	{
+		if (n <= 0)
+		{
+			_puchar('\n');
+			break;
+		}
+		else
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+}
