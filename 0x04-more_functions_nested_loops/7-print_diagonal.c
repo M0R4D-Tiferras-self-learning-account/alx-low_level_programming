bad2e18d21e2da2c0546444ae10774ceff76eacb
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-14 07:15:47 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - A function that print a diagonal
+ *
+ * @n: an int will be handled
+ *
+ * Return: void
+*/
+
+void	print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('\');
+		_putchar('\n');
+
+	_putchar('\n');
+}
