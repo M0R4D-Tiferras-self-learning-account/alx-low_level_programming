ea76936819b0e15540723e36a09fd5d5ab1492d9
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-14 06:34:30 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+*/
+
+void	more_numbers(void)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	c = 1;
+	while (c <= 10)
+	{
+		while (i <= 14 && i >= 0)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+		c++;
+	}
+}
