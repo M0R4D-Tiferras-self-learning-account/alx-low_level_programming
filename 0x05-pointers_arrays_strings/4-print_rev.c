0846f0e9bac0f7b87c5e7d48c43f91720fb96a6c
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-18 12:46:08 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ *
+ * @s: a string will be handled
+ *
+ * Return: The length of the string
+*/
+
+int	_strlen(char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * print_rev - printing str in reverse order
+ *
+ * @s: will be printed in reverse
+ *
+ * Return: void
+*/
+
+void	print_rev(char *s)
+{
+	int	rev;
+
+	rev = _strlen(s);
+	while (s[rev] >= 0)
+	{
+		write(1, &s[rev], 1);
+		rev--;
+	}
+	write(1, "\n", 1);
+}
