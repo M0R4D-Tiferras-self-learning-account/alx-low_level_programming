0ff19bbf386e9be1ab7dd357c906370d772275e3
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-20 13:04:13 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,11 +14,12 @@
 
 char	*_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; src[i] && i < n; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	for (i = 0; dest[i]; i++)
+		;
+	for (j = 0; src[j] && j < n; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
 	return (dest);
 }
