470471ac6de28af4039efb8ce847370b1a674ded
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-20 12:45:13 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * _strncpy - function that concatenates two strings.
+ * _strncpy - function that copy two strings.
  *
  * @dest: a string will be handled
  *
@@ -16,30 +16,18 @@
 char	*_strncpy(char *dest, char *src, int n)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
-
-	if ((dest[j] && src[i]) != '\0')
-	{
-		return (NULL);
-	}
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 
 	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
-	while (dest[j] != '\0')
+	while (dest[i] != '\0')
 	{
-		dest[j] = '\0';
-		j++;
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
