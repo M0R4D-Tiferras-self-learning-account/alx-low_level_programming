e9cfc814d21bd92dc19760b00d286e950764d9da
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-20 09:42:02 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,4 +2,40 @@
 #include <unistd.h>
 
 /**
- * */
+ * _strncat - function that concatenates two strings.
+ *
+ * @dest: a string will be handled
+ *
+ * @src: a string will be handled
+ *
+ * @n: size of the copied charachters
+ *
+ * Return: a charachter
+*/
+
+char	*_strncat(char *dest, char *src, int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	while (dest[j] != '\0')
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
