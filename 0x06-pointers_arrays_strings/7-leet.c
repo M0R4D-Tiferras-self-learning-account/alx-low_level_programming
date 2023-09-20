fc022f4091d9a7ea951098809af7d64c1df853fd
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-20 16:08:25 -0700

    leetspeak

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into leetspeak 1337
+ *
+ * @s: the string that i will encode it
+ *
+ * Return: the s string
+*/
+
+char	*leet(char *s)
+{
+	int	i;
+
+	i = 0;
+
+	while (s[i] != '0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		else if (s[i] == 'e' || s[i] == 'E')
+			s[i] == '3';
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+		else
+			i++;
+	}
+
+	return (s);
+}
