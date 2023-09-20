0cb2597fbc80b750e3c3c1562a497e03f80f9eab
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-20 13:42:45 -0700

    str_rev

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @s: string will be handled
+ *
+ * * Return: a strring
+*/
+
+char	*string_toupper(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'z' && s[i] >= 'a')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
