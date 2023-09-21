2e4d234ff91be27d3cc195da24da580fea9ae9a9
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-21 16:38:53 -0700

    alx-dsa

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,25 +8,17 @@
  * Return: the s string
 */
 
-char	*leet(char *s)
+char *leet(char *s)
 {
-	int	i;
+	int i, j;
+	char *decoding = "aAeEoOtTlL";
+	char *encoding = "4433007711";
 
-	i = 0;
-
-	while (s[i] != '0')
+	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-		i++;
+		for (j = 0; decoding[j]; j++)
+			if (s[i] == decoding[j])
+				s[i] = encoding[j];
 	}
 
 	return (s);
