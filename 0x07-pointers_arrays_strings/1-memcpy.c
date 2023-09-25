fbc58bd5df6a8cf869c619f2f0cc83e4aaf52b2e
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-09-25 11:04:39 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memeory area
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * @n: n bytes to copies
+ *
+ * Return: pointer to dest
+*/
+
+char	_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	in;
+
+	in = 0;
+	while (in < n)
+	{
+		dest[in] = src[in];
+		in++;
+	}
+
+	return (dest);
+}
