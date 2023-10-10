c9f0690c9217a70a66081442fa165017fcd4396a
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-10-09 18:05:02 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,15 +9,18 @@
  * @s2: string 2
  *
  * @n: size of byte to cat in s2
+ *
+ * Return: pointer to concated str
 */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char	*string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *result;
 	unsigned int i, j, s1_len, s2_len;
 
-	s1 == (NULL ? s1 = "" : s1);
-	s2 == (NULL ? s2 = "" : s2);
+	s1 == NULL ? s1 = "" : s1;
+	s2 == NULL ? s2 = "" : s2;
+
 
 	for (s1_len = 0; s1[s1_len]; s1_len++)
 		;
@@ -25,6 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 
 	n >= s2_len ? n = s2_len : n;
+
 
 	result = malloc(sizeof(char) * (s1_len + n + 1));
 	if (result == NULL)
