20cf68981e32944f14db16866245db6492bc33fc
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-10-11 16:03:48 -0700

    function_pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+/**
+ * print_name - function to print name
+ *
+ * @name: name to be printed
+ *
+ * @f: function pointer
+ *
+ * Return: void
+*/
+
+void	print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
