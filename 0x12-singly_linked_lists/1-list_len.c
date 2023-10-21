dc33aadd8f52f29d75a1c8ffbad40fc4e801ffe4
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-10-20 18:48:12 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * list_len - calcul lengh
+  *
+  * @h: a linked list will be handled
+  *
+  * Return: The number of elements of a list
+*/
+
+size_t	list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
