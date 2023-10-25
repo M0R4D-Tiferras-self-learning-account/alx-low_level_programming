5802d7baa1bdbc97868db83d6f618463bf4d9aab
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-10-24 17:53:23 -0700

    single_liked_list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a linked list
+ *
+ * @head: head
+ *
+ * @index: node index
+ *
+ * Return: nth node of a list
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index && head != NULL; i++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+}
