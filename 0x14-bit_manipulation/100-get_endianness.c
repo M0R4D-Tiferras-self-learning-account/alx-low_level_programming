94ef2ea1115fa2008fd06584ea4f6a4cfc974e9b
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-10-27 18:25:02 -0700

    manupulating bit's

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - func to checks the endianness
+ *
+ * Return: 0 if big endian, else 1
+*/
+
+int	get_endianness(void)
+{
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}
